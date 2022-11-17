7810d5b04c68a53a820c9bcb2b20b74b489c57a2
Nerdbisque <113635080+Nerdbisque@users.noreply.github.com>
2022-11-17 14:38:04 -0700

    Update Snake.cs

--- Game/Casting/Snake.cs
+++ Game/Casting/Snake.cs
@@ -111,7 +111,7 @@ namespace Unit05.Game.Casting
                 Point position = new Point(x - i * Constants.CELL_SIZE, y);
                 Point velocity = new Point(1 * Constants.CELL_SIZE, 0);
                 string text = i == 0 ? "8" : "#";
-                Color color = i == 0 ? Constants.YELLOW : Constants.GREEN;
+                Color color = i == 0 ? GetColor() : GetColor();
 
                 Actor segment = new Actor();
                 segment.SetPosition(position);
